7d751a08513eab2023e35d21a11f976622576feb
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-27 08:34:01 +0800

    Render all components in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+import Navlink from "./components/Navlink";
+import Projects from "./components/Projects";
+
 function App() {
 	return (
 		<>
-			<h1>hi</h1>
+			<Navlink />
+			<Hero />
+			<About />
+			<Projects />
+			<Contact />
+			<Footer />
 		</>
 	);
 }
