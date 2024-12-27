c7316c9d9480c93cfb56c7d63248143413d15bd1
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-27 15:20:21 +0800

    Create Header structure and styles

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import profilePicture from "../assets/1720.png";
 
 function Hero() {
 	return (
-		<section className="wrapper">
-			<div>Hero</div>
+		<section className="wrapper full-height shadow " id="hero">
+			<div className="left">
+				<h1 className="hero-heading">Fullstack Developer</h1>
+				<p className="hero-description">
+					Hi! I'm Syakir. Fullstack developer based in Malaysia
+				</p>
+				<div className="tech-stack">
+					<img
+						src="https://skillicons.dev/icons?i=html,css,express,js,nodejs,react,postgres,mongodb"
+						alt=""
+					/>
+				</div>
+			</div>
+			<div className="right">
+				<img src={profilePicture} alt="Profile picture" />
+			</div>
 		</section>
 	);
 }
