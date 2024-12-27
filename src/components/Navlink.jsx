e0e7e177ffb1f44b47ba88313cb5559b0f981459
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-27 10:45:01 +0800

    Create Header structure and styles

--- src/components/Navlink.jsx
+++ src/components/Navlink.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 function Navlink() {
-	return <div>Navlink</div>;
+	return (
+		<header className="wrapper shadow bottom">
+			<div className="logo">
+				<h1>Syakir</h1>
+			</div>
+			<nav>
+				<ul>
+					<li>
+						<a href="#home">Home</a>
+					</li>
+					<li>
+						<a href="#about">About</a>
+					</li>
+					<li>
+						<a href="#projects">Projects</a>
+					</li>
+					<li>
+						<a href="#Contact">Contact</a>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default Navlink;
