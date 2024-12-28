3c3894a97b1d4ac59b61b340f837a6c34d6544ec
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-29 07:55:30 +0800

    Change the paragraph content and style it

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,9 +4,23 @@ function About() {
 	return (
 		<section className="wrapper full-height shadow" id="about">
 			<h2>About Me</h2>
-			{/* //TODO Change the paragraph text */}
 			<div className="about-description">
-				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A quo harum reprehenderit quod, enim, perferendis asperiores quibusdam fuga tenetur quas in eaque officiis incidunt, optio itaque vero commodi voluptatibus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eveniet ea incidunt cupiditate laborum adipisci voluptas quibusdam commodi! Recusandae, cupiditate quisquam quaerat a aut eos maiores quis aliquid temporibus iste! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, ad?</p>
+				<p>
+					Hello! I'm an aspiring full-stack developer passionate about crafting
+					seamless user experiences and robust backend systems. With a keen
+					interest in both frontend and backend development, I thrive on
+					bridging the gap between design and functionality. Through hands-on
+					mini projects, I’ve gained experience with modern JavaScript libraries
+					and frameworks, including React.js and Express.js. These projects have
+					allowed me to design, develop, and deploy web applications that
+					reflect real-world scenarios. I’m also well-versed in deploying
+					websites on cloud platforms like Render, and I’ve worked with AWS EC2
+					instances to set up and manage virtual machines. My technical toolkit
+					includes a strong understanding of version control systems like Git
+					and GitHub, ensuring efficient collaboration and code management. I’m
+					excited to continue learning, building, and contributing to projects
+					that make an impact in the tech world!
+				</p>
 			</div>
 		</section>
 	);
