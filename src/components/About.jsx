d1a95ea7c672868d3c2de81e14679f5fb52b77fe
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-28 23:15:26 +0800

    Create About component structure and styles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,8 +2,12 @@ import React from "react";
 
 function About() {
 	return (
-		<section className="wrapper">
-			<div>About</div>
+		<section className="wrapper full-height shadow" id="about">
+			<h2>About Me</h2>
+			{/* //TODO Change the paragraph text */}
+			<div className="about-description">
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. A quo harum reprehenderit quod, enim, perferendis asperiores quibusdam fuga tenetur quas in eaque officiis incidunt, optio itaque vero commodi voluptatibus eligendi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum eveniet ea incidunt cupiditate laborum adipisci voluptas quibusdam commodi! Recusandae, cupiditate quisquam quaerat a aut eos maiores quis aliquid temporibus iste! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt, ad?</p>
+			</div>
 		</section>
 	);
 }
