0a4842008518d99c85dd05b1dd72fa4f32deac32
Syakir Haziq <115148301+tadanama@users.noreply.github.com>
2024-12-29 15:04:05 +0800

    Created Footer component structure and styles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,23 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 
 function Footer() {
 	return (
-		<section>
-			<div>Footer</div>
-		</section>
+		<footer className="wrapper flex">
+			<p>© Syakir Haziq - All Rights Reserved</p>
+			<div className="socials">
+				<a
+					href="https://www.linkedin.com/in/muhammad-syakir-haziq-mohd-nordin-a7b78818b"
+					target="_blank"
+				>
+					<FontAwesomeIcon icon={faLinkedin} className="icon" />
+				</a>
+				<a href="https://github.com/tadanama" target="_blank">
+					<FontAwesomeIcon icon={faGithub} className="icon" />
+				</a>
+			</div>
+		</footer>
 	);
 }
 
